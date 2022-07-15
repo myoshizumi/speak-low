0548df34d72e47d04389987478c39618cb57391b
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2022-07-15 21:27:39 +0900

    Add DI(TweetService)

--- app/Http/Controllers/Tweet/IndexController.php
+++ app/Http/Controllers/Tweet/IndexController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Tweet;
 
 use App\Http\Controllers\Controller;
 use App\Models\Tweet;
+use App\Services\TweetService;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
@@ -14,9 +15,11 @@ class IndexController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function __invoke(Request $request)
+    public function __invoke(Request $request, TweetService $tweetService)
     {
-        $tweets = Tweet::orderBy('created_at', 'DESC')->get();
+        // $tweets = Tweet::orderBy('created_at', 'DESC')->get();
+        // $tweetService = new TweetService();
+        $tweets = $tweetService->getTweets();
         return view('tweet.index')
             ->with('tweets', $tweets);
     }
