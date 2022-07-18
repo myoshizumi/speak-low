d6810515a55be63cd34db1ca7b60b428763b888a
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2022-07-19 02:23:21 +0900

    Add mail function by using MailHog

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -2,14 +2,16 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
 use App\Models\User;
-use App\Providers\RouteServiceProvider;
-use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rules;
+use App\Http\Controllers\Controller;
+use App\Mail\NewUserIntroduction;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\Rules;
+use Illuminate\Contracts\Mail\Mailer;
+use Illuminate\Auth\Events\Registered;
+use App\Providers\RouteServiceProvider;
 
 class RegisteredUserController extends Controller
 {
@@ -31,7 +33,7 @@ class RegisteredUserController extends Controller
      *
      * @throws \Illuminate\Validation\ValidationException
      */
-    public function store(Request $request)
+    public function store(Request $request, Mailer $mailer)
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
@@ -39,16 +41,22 @@ class RegisteredUserController extends Controller
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
-        $user = User::create([
+        $newUser = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
 
-        event(new Registered($user));
+        event(new Registered($newUser));
 
-        Auth::login($user);
+        Auth::login($newUser);
 
+
+        $allUser = User::get();
+        foreach ($allUser as $user) {
+            $mailer->to($user->email)
+                ->send(new NewUserIntroduction($user, $newUser));
+        }
         return redirect(RouteServiceProvider::HOME);
     }
 }
