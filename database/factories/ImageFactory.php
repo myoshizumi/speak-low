291e195dc1ee4c1da9dbee295ae007bfcda694ef
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2022-07-20 08:30:55 +0900

    Add file uploads for a tweet

---
+++ database/factories/ImageFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Image>
+ */
+class ImageFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        if (!Storage::exists('public/images')) {
+            Storage::makeDirectory('public/images');
+        }
+        return [
+            'name' => $this->faker->image(
+                storage_path('app/public/images'),
+                640,
+                480,
+                null,
+                false
+            )
+        ];
+    }
+}
--- database/seeders/TweetsSeeder.php
+++ database/seeders/TweetsSeeder.php
@@ -2,11 +2,12 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Image;
+use App\Models\Tweet;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Str;
-use App\Models\Tweet;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class TweetsSeeder extends Seeder
 {
@@ -18,7 +19,13 @@ class TweetsSeeder extends Seeder
     public function run()
     {
 
-        Tweet::factory()->count(10)->create();
+        Tweet::factory()->count(10)->create()->each(
+            fn ($tweet) =>
+            Image::factory()->count(4)->create()->each(
+                fn ($image) =>
+                $tweet->images()->attach($image->id)
+            )
+        );
     }
 
     // public function run()
